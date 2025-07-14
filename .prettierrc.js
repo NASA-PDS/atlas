600bf0faac64ee3d6fe2b06fcd404551e68a96af
Anil Natha <Anil.Natha@jpl.nasa.gov>
2025-07-14 11:04:46 -0700

    Removed deprecated package that provided default prettier config. Merged prettier config from previously referenced package with our local overrides inside .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,24 @@
 module.exports = {
-    ...require('@werkzeugkiste/prettier-config'),
-    // add your overrides here
-    endOfLine: 'auto',
+    arrowParens: "always",
+    printWidth: 100,
+    quoteProps: "consistent",
     semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "es5",
+    overrides: [
+      {
+        "files": ["*.json", "*.yml", "*.md"],
+        "options": {
+          "tabWidth": 2
+        }
+      },
+      {
+        "files": "*.{ts,tsx}",
+        "options": {
+          "parser": "typescript"
+        }
+      }
+    ],
+    endOfLine: 'auto'
 };

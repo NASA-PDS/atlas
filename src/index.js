7d6993083bac80d97a07a4471f37944b7ad2bb3f
Anil Natha <Anil.Natha@jpl.nasa.gov>
2025-08-12 10:05:05 -0700

    Fixed breaking change with root element rendering when upgrading to React v18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 
 import { Provider } from 'react-redux'
 import { store } from './core/redux/store/store'
@@ -16,13 +16,15 @@ import './index.css'
 
 window.token = 'token'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <Provider store={store}>
         <ThemeProvider theme={muiTheme}>
             <MuiPickersUtilsProvider utils={MomentUtils}>
                 <AppRoutes />
             </MuiPickersUtilsProvider>
         </ThemeProvider>
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 )

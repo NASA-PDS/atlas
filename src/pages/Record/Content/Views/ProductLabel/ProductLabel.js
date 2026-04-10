0f9e1f002b95ded9aa4bd9e3b3fac38d8a28668f
Anil Natha <Anil.Natha@jpl.nasa.gov>
2026-04-10 11:46:06 -0700

    Fixed an issue with an improperly named prop for the SimpleTreeView component on the product detail page's product label view

--- src/pages/Record/Content/Views/ProductLabel/ProductLabel.js
+++ src/pages/Record/Content/Views/ProductLabel/ProductLabel.js
@@ -559,9 +559,10 @@ const ProductLabel = (props) => {
                     </div>
                     <div className={c.bottom}>
                         <SimpleTreeView
-                            defaultExpanded={Array(labelTree.numOfKeys)
+                            defaultExpandedItems={Array(labelTree.numOfKeys)
                                 .fill()
-                                .map((x, i) => String(i))}
+                                .map((x, i) => String(i))
+                            }
                         >
                             {labelTree.tree}
                         </SimpleTreeView>
